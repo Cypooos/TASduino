16a8a859e969fe7496c68134fa2f8c48391a5fb4
Discursif <60020572+Discursif@users.noreply.github.com>
2020-05-05 23:04:40 +0200

    Adding detailed step to the compiler

--- core/Compiler.py
+++ core/Compiler.py
@@ -5,11 +5,15 @@ class Compiler():
 
   def __init__(self,**kwargs):
     self.options = kwargs
-    self.path = kwargs.get("compiler_path","core/compiler/")
 
-  def sendProgram(self):
-    pass
+  def sendProgram(self,name):
+    os.system("sudo dfu-programmer atmega16u2 erase") # changing param. 
+    os.system("sudo dfu-programmer atmega16u2 flash core/compiler/"+name+".hex")
+    os.system("sudo dfu-programmer atmega16u2 reset")
 
   def compileProgram(self,program):
-    os.system("make -C core/LUFA")
-    os.system("make -C core/compiler")
+    # change makefile with configuration
+    os.system("make -C core/LUFA") # if not already build. Silence the shell
+    os.system("make -C core/compiler") # Silence the shell. 
+    # move and rename output file (.hex) to compiled/
+    # delete .bin .eep .elf .lss .map .sym
